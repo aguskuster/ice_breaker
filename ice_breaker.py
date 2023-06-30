f5cbfc2a6eab2be467e9ef8b8f8ec62966261a9b
Agustin Kuster <akusterpiriz@gmail.com>
2023-06-30 09:22:19 -0300

    Linkedin scraping mix with open IA

--- ice_breaker.py
+++ ice_breaker.py
@@ -2,13 +2,8 @@ from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
-information = """
-Masashi Kishimoto nació en la prefectura de Okayama, Japón el 8 de noviembre de 1974, junto a su hermano gemelo menor, Seishi Kishimoto. Durante su infancia, Kishimoto mostró interés por los personajes de dibujo animado.
+from third_parties.linkedin import scrape_linkedin_profile
 
-Fue en este momento de su vida en que Masashi Kishimoto comenzó a pensar que el manga era genial y deseaba convertirse en un famoso mangaka como Akira Toriyama creando su primer manga cerca de este momento y que llevaba por título "Hiatari-kun", una historia que giraba acerca de “un niño ninja de las sombras”.
-
-En la escuela intermedia, Kishimoto comenzó a enfocarse en otras cosas diferentes al dibujo. El béisbol se convirtió en gran parte de su vida, y naturalmente tenía que dedicar más tiempo al estudio, lo que significó poco o ningún tiempo para dibujar. Se comenzó a preocupar de si estaba “muy viejo para dibujar”, y en este punto un evento increíble ocurrió en su vida.
-"""
 if __name__ == "__main__":
     print("Hello LangChain")
 
@@ -26,4 +21,8 @@ if __name__ == "__main__":
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    print(chain.run(information=information))
+    linkedin_data = scrape_linkedin_profile("Eden Marco Linkedin Profile")
+    
+    print(chain.run(information=linkedin_data))
+
+
